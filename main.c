5b92e2638a1aac13ad743813fa5d3560ab7da8e6
kev43 <kev43200@gmail.com>
2017-02-27 17:17:54 +0100

    Ajout des commentaires

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/********************************************************************/
+/*** VIVIER David													*/
+/*** VARAGNAT Kévin													*/
+/*** 27/02/2017											            */
+/*** Exécuter ./main pour utiliser le programme 					*/
+/*** Make clean pour supprimer l'éxécutable et les fichiers .o      */																*/
+/********************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -5,24 +13,24 @@
 
 int main() {
 
-	int tab[] = {3,5,6,2,8,9,11,7,4,10,12,15,20,1,19,13,17};
+	int tab[] = {3,5,6,2,8,9,11,7,4,10,12,15,20,1,19,13,17}; // tableau qui contient les valeurs intitiales
 	int taille_tab = 17;
 
 
 	int tab_tas[18] ; // Creation du tableau qui sert à stocker le tas
-	tab_tas[0] = 0;
+	tab_tas[0] = 0; // la case d'indice 0 sert à stocker le nombre d'éléments dans le tas
 
 	int i = 0;
 	for (i = 0; i < taille_tab; i++) {
 		//printf("insertion de %d\n", tab[i]);
-		insertion(tab_tas, tab[i]);
+		insertion(tab_tas, tab[i]); // on insert tous les éléments dans le tas
 	}
 
 	printf("tas construit : ");
 	afficher(tab_tas);
 
 	for (i = 1; i <= taille_tab; i++) {
-		int valeur = destruction(tab_tas);
+		int valeur = destruction(tab_tas); // on détruit les éléments du tas un par un
 		printf("valeur retiree : %d\n", valeur);
 	}
 
--- tas.h
+++ tas.h
@@ -2,18 +2,54 @@
 #ifndef INCLUDE_TAS_H
 #define INCLUDE_TAS_H
 
+/*
+*	entrée : entier - indice de l'élément courant
+*	sortie : entier - indice du père de l'élément courant
+*	Calcule l'indice du père dans le tableau à partir de l'indice courant
+*/
 int indicePere(int i);
 
+/*
+*	entrée : entier - indice de l'élément courant
+*	sortie : entier - indice du fils gauche de l'élément courant
+*	Calcule l'indice du fils gauche dans le tableau à partir de l'indice courant
+*/
 int indiceGauche(int i);
 
+/*
+*	entrée : entier - indice de l'élément courant
+*	sortie : entier - indice du fils droite de l'élément courant
+*	Calcule l'indice du fils droit dans le tableau à partir de l'indice courant
+*/
 int indiceDroite(int i);
 
+/*
+*	entrée/sortie : tableau d'entier  tab - tableau qui contient le tas
+*	entrée : entier indice1 - l'indice du premier élément à échanger
+*	entrée : entier indice2 - l'indice du deuxième élément à échanger
+*	Echange deux éléments dans le tas
+*/
 void echange(int *tab, int indice1, int indice2);
 
+/*
+*	entrée : tableau d'entier  tab - tableau qui contient le tas
+*	sortie : Affiche le tableau qui contient le tas (le premier élément du tableau correspond à sa taille)
+*	Affiche le tableau qui contient le tas (le premier élément du tableau correspond à sa taille)
+*/
 void afficher(int *tab);
 
+/*
+*	entrée/sortie : tableau d'entier  tab - tableau qui contient le tas
+*	entrée : entier valeur - contient la valeur à insérer dans le tas
+*	Insère un élément dans le tas à la bonne position
+*/
 void insertion(int *tab, int valeur);
 
+/*
+*	entrée/sortie : tableau d'entier  tab - tableau qui contient le tas
+*	sortie : entier - la valeur de l'élément supprimé
+*	Supprime l'élément au sommet du tas et le réorganise
+*/
 int destruction(int *tab);
 
 #endif
