baccd0057b4acb1c31f18cd2040ae9124f23d77c
David Vivier <contact@davidvivier.fr>
2017-02-27 16:09:43 +0100

    Creation of heap from tab - works

--- main.c
+++ main.c
@@ -5,18 +5,21 @@
 
 int main() {
 
-	int tab[17] ; // Creation du tableau qui sert à stockert le tas
-	tab[0] = 0;
+	int tab[] = {3,5,6,2,8,9,11,7,4,10,12,15,20,1,19,13,17};
+	int taille_tab = 17;
 
 
-	
-	insertion(tab , 3);
-	insertion(tab , 5);
-	insertion(tab , 6);
-	insertion(tab , 2);
+	int tab_tas[18] ; // Creation du tableau qui sert à stockert le tas
+	tab_tas[0] = 0;
+
+	int i = 0;
+	for (i = 0; i < taille_tab; i++) {
+		printf("insertion de %d\n", tab[i]);
+		insertion(tab_tas, tab[i]);
+	}
 
 	printf("tableau final : ");
-	afficher(tab);
+	afficher(tab_tas);
 
 	return 0;
 }
